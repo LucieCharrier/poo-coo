c3913b242f2d8ecf119719c811b67ee261ee3b89
LucieCharrier <lucie.charrier@cpe.fr>
2020-04-03 15:04:52 +0200

    Avancement 3 avril 2020

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -13,19 +13,19 @@ public abstract class AbstractPiece implements Pieces {
 	}
 	
 	public int getX() {
-		
+		return this.coord.x;
 	}
 	
 	public int getY() {
-		
+		return this.coord.y;
 	}
 	
 	public Couleur getCouleur() {
-		
+		return this.couleur;
 	}
 	
 	public boolean move(int x, int y) {
-		
+		return false; //default
 	}
 	
 	public boolean capture() {
@@ -33,11 +33,20 @@ public abstract class AbstractPiece implements Pieces {
 	}
 	
 	public abstract boolean isMoveOk(int xFinal, int yFinal) {
-		
+ //default
 	}
 
 	@Override 
 	public java.lang.String toString(){
+		return "Nom de la pièce :" + this.nom + "\n" +
+				"Colonne :" + this.getX() + "\n" +
+				"Ligne :" + this.getY();
 		
+	}
+	
+	public static void main(String[] args) {
+		Pieces maTour = new Tour(Couleur.NOIR, new Coord(0, 0));
+		Couleur a = maTour.getCouleur();
+		System.out.println(a);
 	}
 }
